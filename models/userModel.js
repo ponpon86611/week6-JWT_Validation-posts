10ad409f3be929de5fb3346d0269e4ccd4dd73cd
Pon <blackdog755308@gmail.com>
2022-06-04 12:13:51 +0800

    調整排版與增加versionkey的設定

--- models/userModel.js
+++ models/userModel.js
@@ -13,35 +13,40 @@ const userMessage = {
     }
 }
 
-const userSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: [true, userMessage.name.validError]
-    },
-    email: {
-      type: String,
-      required: [true, userMessage.email.validError],
-      unique: true,
-      lowercase: true,
-      select: false
-    },
-    photo: String,
-    sex: {
-      type: String,
-      enum: ['male','female']
-    },
-    password: {
-      type: String,
-      required: [true, userMessage.password.validError],
-      minlength: 8,
-      select: false
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      select: false
-    }
-  });
+const userSchema = new mongoose.Schema(
+  {
+      name: {
+        type: String,
+        required: [true, userMessage.name.validError]
+      },
+      email: {
+        type: String,
+        required: [true, userMessage.email.validError],
+        unique: true,
+        lowercase: true,
+        select: false
+      },
+      photo: String,
+      sex: {
+        type: String,
+        enum: ['male','female']
+      },
+      password: {
+        type: String,
+        required: [true, userMessage.password.validError],
+        minlength: 8,
+        select: false
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        select: false
+      }
+  },
+  {
+      versionKey: false
+  }
+);
 
 const User = mongoose.model('user', userSchema);
 
